42f4b6a1a8007967e1d25f8826f6d6af0b8c5393
AlanAndray01 <syedarslanshah7861@gmail.com>
2025-10-04 14:49:59 +0500

    new commit

---
+++ home.js
@@ -0,0 +1,4 @@
+// Drop down enu toggle hai yai
+document.getElementById("searchToggle").addEventListener("click", () => {
+  document.getElementById("searchDropdown").classList.toggle("show");
+});
--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 function toggleMenu() {
   document.querySelector("ul").classList.toggle("active");
 }
+
+// Login page per janai ka liyai js
+
+document.getElementById("loginBtn").addEventListener("click", () => {
+  window.location.href = "login.html"; // Redirect to login
+});
+
+document.getElementById("signupBtn").addEventListener("click", () => {
+  window.location.href = "signup.html"; // Redirect to signup
+});
